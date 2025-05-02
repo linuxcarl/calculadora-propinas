cf3ec950f0740da563a0f597b7dd2e47bf0f70ee
Carlos Ramirez <carlos@extrasistemas.com>
2025-05-02 08:33:25 -0600

    base code for project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <header className="bg-teal-500 py-5">
+      <h1 className="text-center text-4xl font-black"> Calculadora de propinas y consumo</h1>
+      </header> 
+    </>
+  )
+}
+
+export default App
